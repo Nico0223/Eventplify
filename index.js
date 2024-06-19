a1499ba6647593a897a32214131ee86dae398624
1Jerome23 <jerome_victoria@dlsu.edu.ph>
2024-06-19 19:52:18 +0800

    signup is ok

--- index.js
+++ index.js
@@ -38,18 +38,15 @@ var hbs = require("hbs");
 app.set("view engine", "hbs");
 hbs.registerPartials(path.join(__dirname, "views", "partials"));
 
-app.use("/", todoRouter);
-app.use("/user", userRouter);
+// app.use("/", todoRouter); ///api/todo?
+app.use("/api/users", userRouter);
 
 /* For file uploads */
 const fileUpload = require("express-fileupload");
 app.use(fileUpload()); //initialize file upload middleware
 
-app.get("/", async (req, res) => {
-  res.redirect("/todo");
-  //res.sendFile(path.join(__dirname, "public", "home.html"));
-  var user = await User.findById("6670e38a9dc29d82305a675f");
-  console.log(user);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'home_unlogged.html'));
 });
 
 var server = app.listen(port, "0.0.0.0", function () {
--- javascript/user.js
+++ javascript/user.js
@@ -1,77 +1,75 @@
 $(document).ready(function() {
-    // User registration logic
-    $('#signup_form').on('submit', function(event) {
-      event.preventDefault();
-      const username = $('#signup_username').val();
-      const password = $('#signup_password').val();
-      const email = $('#signup_email').val();
-  
-      // Send a POST request to the server for registration
-      fetch('/user', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, username, password }),
-      })
-        .then(response => {
-          if (response.ok) {
-            alert('Registration successful');
-          } else {
-            response.json().then(data => {
-              alert(data.error);
-            });
-          }
-        })
-        .catch(error => {
-          console.log(error);
-          alert('An error occurred during registration');
-        });
-    });
-    
-    // User login logic
-    $('#login_form').on('submit', function (event) {
-      event.preventDefault();
-      const username = $('#login_username').val();
-      const password = $('#login_password').val();
-      // Send a POST request to the server for login
-      fetch('/user/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password }),
-      })
-        .then(response => response.json())
-        .then(data => {
-          if (data.message === 'Logged in!') {
-            alert('Login successful');
-            window.location.href = 'home.html'; 
-          } else {
-            alert('Invalid login credentials');
-          }
-        })
-        .catch(error => {
-          console.log(error);
-          alert('An error occurred during login');
-        });
-    });
-  
-    $('.plan').on('click', function(event) {
-      event.preventDefault();
-      redirectToHomePage();
-    });
-      
-    async function redirectToHomePage() {
-      const response = await fetch('/user/is-authenticated');
-      const data = await response.json();
-      if (data.authenticated) {
-        window.location.href = 'home.html';
-  
+  // User registration logic
+  $('#signup_form').on('submit', function(event) {
+    event.preventDefault();
+    const username = $('#signup_username').val();
+    const password = $('#signup_password').val();
+    const email = $('#signup_email').val();
+
+    // Send a POST request to the server for registration
+    fetch('/api/users/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, username, password }),
+    })
+    .then(response => {
+      if (response.ok) {
+        alert('Registration successful');
       } else {
-        alert('Please register and log in to access the task page.');
+        response.json().then(data => {
+          alert(data.error);
+        });
       }
-    }
-  
-  
+    })
+    .catch(error => {
+      console.log(error);
+      alert('An error occurred during registration');
+    });
   });
+
+  // User login logic
+  $('#login_form').on('submit', function(event) {
+    event.preventDefault();
+    const username = $('#login_username').val();
+    const password = $('#login_password').val();
+
+    // Send a POST request to the server for login
+    fetch('/api/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.message === 'Logged in!') {
+        alert('Login successful');
+        window.location.href = 'home.html'; 
+      } else {
+        alert('Invalid login credentials');
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      alert('An error occurred during login');
+    });
+  });
+
+  $('.plan').on('click', function(event) {
+    event.preventDefault();
+    redirectToHomePage();
+  });
+
+  async function redirectToHomePage() {
+    const response = await fetch('/api/users/is-authenticated');
+    const data = await response.json();
+    if (data.authenticated) {
+      window.location.href = 'home.html';
+    } else {
+      alert('Please register and log in to access the task page.');
+    }
+  }
+});
