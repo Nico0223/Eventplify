ee1eb364260b9f1be98173aa051810c269a050da
1Jerome23 <jerome_victoria@dlsu.edu.ph>
2024-07-06 17:43:10 +0800

    can add guest

--- models/Guest.js
+++ models/Guest.js
@@ -1,15 +1,26 @@
-const mongoose = require("mongoose");
+// Guest.js
+const mongoose = require('mongoose');
 
 const GuestSchema = new mongoose.Schema({
   table: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "Table",
+    ref: 'Table', // Assuming 'Table' model exists and is correctly referenced
   },
-  name: String,
-  category: String,
-  status: String,
+  name: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: false, // Adjust as per your schema
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: ['Pending', 'Not Going', 'Going'], // Assuming these are your valid statuses
+  },
 });
 
-const Guest = mongoose.model("Guest", GuestSchema);
+const Guest = mongoose.model('Guest', GuestSchema);
 
 module.exports = Guest;
