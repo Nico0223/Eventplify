a407d9a88e6d652c3408c52c55a6a739a45b046d
xmeryllex <merylle_salvador@dlsu.edu.ph>
2024-07-15 01:28:05 +0800

    changed chat message time to PH time

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -2,6 +2,7 @@ const Event = require("../models/Event");
 const Chat = require("../models/Chat");
 const User = require("../models/User");
 const bodyParser = require("body-parser");
+const { format, utcToZonedTime } = require('date-fns-tz');
 
 exports.addChat = async (req, res) => {
   const { chatname, members } = req.body;
@@ -55,12 +56,9 @@ exports.addMessage = async (req, res) => {
 
 function getCurrentTimeFormatted() {
   const now = new Date();
-  const year = now.getFullYear();
-  const month = String(now.getMonth() + 1).padStart(2, "0");
-  const day = String(now.getDate()).padStart(2, "0");
-  const hours = String(now.getHours()).padStart(2, "0");
-  const minutes = String(now.getMinutes()).padStart(2, "0");
-  const seconds = String(now.getSeconds()).padStart(2, "0");
+  const pstTime = utcToZonedTime(now, 'Asia/Manila'); // Convert UTC to Philippine Time
 
-  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  const formattedTime = format(pstTime, 'yyyy-MM-dd HH:mm:ss');
+  return formattedTime;
+
 }
