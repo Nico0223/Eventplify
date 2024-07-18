dab3b090a2663d0b348334f27d3d751b76c00130
1Jerome23 <jerome_victoria@dlsu.edu.ph>
2024-07-18 23:28:15 +0800

    1

--- models/Collaborator.js
+++ models/Collaborator.js
@@ -1,33 +1,21 @@
 const mongoose = require('mongoose');
 
 const CollaboratorSchema = new mongoose.Schema({
-  event: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Event',
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-  },
-  role: {
-    type: String,
-    enum: ['Owner', 'Collaborator'],
-    required: true,
-  },
-  canEditBudget: {
-    type: Boolean,
-    default: false,
-  },
-  canEditGuest: {
-    type: Boolean,
-    default: false,
-  },
-  canEditTodo: {
-    type: Boolean,
-    default: false,
-  },
+    event: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Event',
+        required: true
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    name: String,
+    role: {
+        type: String,
+        enum: ['Owner', 'Collaborator'],
+        required: true
+    }
 });
 
-const Collaborator = mongoose.model('Collaborator', CollaboratorSchema);
-
-module.exports = Collaborator;
+module.exports = mongoose.model('Collaborator', CollaboratorSchema);
